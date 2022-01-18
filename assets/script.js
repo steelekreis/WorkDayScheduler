a39dd39783169388fc442a075bb0d84ced5f51ee
steelekreis <steele.kreis@gmail.com>
2022-01-18 00:35:14 -0700

    working on script.js

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,54 @@ var today = moment();;
 $("#date").text(today.format("dddd, MMMM Do YYYY"))
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
+var taskData = {
+    "0": [],
+    "1": [],
+    "2": [],
+    "3": [],
+    "4": [],
+    "5": [],
+    "6": [],
+    "7": [],
+    "8": [],
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": [],
+    "18": [],
+    "19": [],
+    "20": [],
+    "21": [],
+    "22": [],
+    "23": [],
+};
+
+var storeTasks = function() {
+    localStorage.setItem("taskData", JSON.stringify(taskData));
+};
+
+var create = function() {
+    var timeDiv  = $("#" + hour);
+    function (task, timeDiv) {
+        var taskLi = timeDiv.find(".task");
+        var taskDiv = $("<div>")
+            .addClass("")
+    }
+};
+
+var retrieveTasks = function() {
+    var pulledTasks = JSON.parse(localStorage.getItem("taskData"));
+    if (retrieveTasks) {
+        taskData = retrieveTasks
+        
+        $.each(taskData, create)
+    }
+};
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // WHEN I click into a time block
